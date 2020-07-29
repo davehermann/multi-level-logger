bf21ed6829abbf4696d8a6ebb1e7c74befd89214
Dave Hermann <6787333+davehermann@users.noreply.github.com>
2020-07-29 12:25:39 -0400

    Add testing of each level for not outputting lower levels

--- tests/testList.ts
+++ tests/testList.ts
@@ -3,12 +3,12 @@ import { SetLogLevel } from "./testLogging";
 function presetLogLevels(): void {
     describe(`Test 1) Each set level from Dev to Fatal via string`, function() {
         SetLogLevel(`Dev`);
-        // SetLogLevel(`Trace`);
-        // SetLogLevel(`Debug`);
-        // SetLogLevel(`Info`);
-        // SetLogLevel(`Warn`);
-        // SetLogLevel(`Error`);
-        // SetLogLevel(`Fatal`);
+        SetLogLevel(`Trace`);
+        SetLogLevel(`Debug`);
+        SetLogLevel(`Info`);
+        SetLogLevel(`Warn`);
+        SetLogLevel(`Error`);
+        SetLogLevel(`Fatal`);
     });
 }
 
